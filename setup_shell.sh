39dda04eca6553e9cae89e5fa7c952bbea74d366
agiachris <christopheragia@gmail.com>
2022-01-25 11:40:09 -0800

    remove dm_control suite and mujoco
    Signed-off-by: agiachris <christopheragia@gmail.com>

--- setup_shell.sh
+++ setup_shell.sh
@@ -1,8 +1,7 @@
 # Make sure we have the conda environment set up.
 CONDA_PATH=~/miniconda3/bin/activate
-ENV_NAME=research
+ENV_NAME=temporal_policies
 REPO_PATH=path/to/your/repo
-USE_MUJOCO_PY=false
 
 # Setup Conda
 source $CONDA_PATH
@@ -11,22 +10,11 @@ cd $REPO_PATH
 
 unset DISPLAY # Make sure display is not set or it will prevent scripts from running in headless mode.
 
-if $USE_MUJOCO_PY; then
-    echo "Using mujoco_py"
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mujoco200/bin
-fi
-
-# Try to import CUDA if we can, also add mujoco rendering backends. OK if unused.
+# Try to import CUDA if we can.
 if [ -d "/usr/local/cuda-11.1" ]; then
     export PATH=/usr/local/cuda-11.1/bin:$PATH
-    export MUJOCO_GL="egl" 
 elif [ -d "/usr/local/cuda-11.0" ]; then
     export PATH=/usr/local/cuda-11.0/bin:$PATH
-    export MUJOCO_GL="egl" 
 elif [ -d "/usr/local/cuda-10.2" ]; then
     export PATH=/usr/local/cuda-10.2/bin:$PATH
-    export MUJOCO_GL="egl" 
-else
-    echo "Could not find a CUDA version, using CPU."
-    export MUJOCO_GL="osmesa"
 fi
