07cbd4b4a4c5185879b7585a3161008a34c020b4
Toki Migimatsu <toki.migimatsu@gmail.com>
2022-04-15 10:48:26 -0700

    Clean up eval_planners script

--- scripts/eval/eval_planners.sh
+++ scripts/eval/eval_planners.sh
@@ -2,166 +2,96 @@
 
 set -e
 
-DEBUG=0
-
-function eval_planner {
-    ARGS=""
-    ARGS="${ARGS} --planner-config ${PLANNER_CONFIG}"
-    ARGS="${ARGS} --env-config ${ENV_CONFIG}"
-    if [ ! -z "${POLICY_CHECKPOINTS}" ]; then
-        ARGS="${ARGS} --policy-checkpoints ${POLICY_CHECKPOINTS}"
-    fi
-    if [ ! -z "${DYNAMICS_CHECKPOINT}" ]; then
-        ARGS="${ARGS} --dynamics-checkpoint ${DYNAMICS_CHECKPOINT}"
-    fi
-    ARGS="${ARGS} --seed 0"
-    if [[ $DEBUG -ne 0 ]]; then
-        ARGS="${ARGS} --num-eval 1"
-        ARGS="${ARGS} --path plots/pybox2d_debug"
-        ARGS="${ARGS} --verbose 1"
-    else
-        ARGS="${ARGS} --num-eval 100"
-        ARGS="${ARGS} --path plots/pybox2d"
-        ARGS="${ARGS} --verbose 0"
-    fi
-    CMD="python scripts/eval/eval_planners.py ${ARGS}"
+function run_cmd {
     echo ""
     echo "${CMD}"
-    ${CMD}
+    if [[ `hostname` == "sc.stanford.edu" ]]; then
+        sbatch run_juno.sh "${CMD}"
+    else
+        ${CMD}
+    fi
+}
+
+function eval_planner {
+    args=""
+    args="${args} --planner-config ${PLANNER_CONFIG}"
+    args="${args} --env-config ${ENV_CONFIG}"
+    if [ ! -z "${POLICY_CHECKPOINTS}" ]; then
+        args="${args} --policy-checkpoints ${POLICY_CHECKPOINTS}"
+    fi
+    if [ ! -z "${DYNAMICS_CHECKPOINT}" ]; then
+        args="${args} --dynamics-checkpoint ${DYNAMICS_CHECKPOINT}"
+    fi
+    args="${args} --seed 0"
+    if [[ $DEBUG -ne 0 ]]; then
+        args="${args} --num-eval 1"
+        args="${args} --path plots/pybox2d_debug"
+        args="${args} --verbose 1"
+    else
+        args="${args} --num-eval 100"
+        args="${args} --path plots/pybox2d"
+        args="${args} --verbose 0"
+    fi
+    CMD="python scripts/eval/eval_planners.py ${args}"
+    run_cmd
 }
 
 function visualize_results {
-    ARGS=""
-    ARGS="${ARGS} --path plots/pybox2d"
-    METHODS=""
-    METHODS="${METHODS} policy_cem"
-    METHODS="${METHODS} random_cem"
-    METHODS="${METHODS} policy_shooting"
-    METHODS="${METHODS} random_shooting"
-    METHODS="${METHODS} policy_cem_oracle_dynamics"
-    METHODS="${METHODS} random_cem_oracle_dynamics"
-    METHODS="${METHODS} policy_shooting_oracle_dynamics"
-    METHODS="${METHODS} random_shooting_oracle_dynamics"
-    METHODS="${METHODS} policy_cem_oracle_value_dynamics"
-    METHODS="${METHODS} random_cem_oracle_value_dynamics"
-    METHODS="${METHODS} policy_shooting_oracle_value_dynamics"
-    METHODS="${METHODS} random_shooting_oracle_value_dynamics"
-    METHODS="${METHODS} random"
-    ARGS="${ARGS} --methods ${METHODS}"
-    CMD="python scripts/visualize/visualize_planners.py ${ARGS}"
-    echo ""
-    echo "${CMD}"
-    ${CMD}
+    args=""
+    args="${args} --path plots/pybox2d"
+    args="${args} --methods ${PLANNERS[@]}"
+    CMD="python scripts/visualize/visualize_planners.py ${args}"
+    run_cmd
 }
 
-PLANNER_CONFIG=configs/pybox2d/planners/policy_cem.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-eval_planner
+# Setup.
 
-PLANNER_CONFIG=configs/pybox2d/planners/policy_shooting.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-eval_planner
+DEBUG=0
 
-PLANNER_CONFIG=configs/pybox2d/planners/random_cem.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-eval_planner
-
-PLANNER_CONFIG=configs/pybox2d/planners/random_shooting.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-eval_planner
-
-# Oracle value.
-
-# PLANNER_CONFIG=configs/pybox2d/planners/policy_cem_oracle_value.yaml
-# ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-# POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-# DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-# eval_planner
-#
-# PLANNER_CONFIG=configs/pybox2d/planners/policy_shooting_oracle_value.yaml
-# ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-# POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-# DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-# eval_planner
-#
-# PLANNER_CONFIG=configs/pybox2d/planners/random_cem_oracle_value.yaml
-# ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-# POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-# DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-# eval_planner
-#
-# PLANNER_CONFIG=configs/pybox2d/planners/random_shooting_oracle_value.yaml
-# ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-# POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-# DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-# eval_planner
-
-# Oracle dynamics.
-
-PLANNER_CONFIG=configs/pybox2d/planners/policy_cem_oracle_dynamics.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT=""
-eval_planner
-
-PLANNER_CONFIG=configs/pybox2d/planners/policy_shooting_oracle_dynamics.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT=""
-eval_planner
-
-PLANNER_CONFIG=configs/pybox2d/planners/random_cem_oracle_dynamics.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT=""
-eval_planner
-
-PLANNER_CONFIG=configs/pybox2d/planners/random_shooting_oracle_dynamics.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT=""
-eval_planner
-
-# Oracle value and dynamics.
-
-PLANNER_CONFIG=configs/pybox2d/planners/policy_cem_oracle_value_dynamics.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT=""
-eval_planner
-
-PLANNER_CONFIG=configs/pybox2d/planners/policy_shooting_oracle_value_dynamics.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT=""
-eval_planner
-
-PLANNER_CONFIG=configs/pybox2d/planners/random_cem_oracle_value_dynamics.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT=""
-eval_planner
-
-PLANNER_CONFIG=configs/pybox2d/planners/random_shooting_oracle_value_dynamics.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT=""
-eval_planner
-
+declare -a PLANNERS=(
+# Q-value / Latent dynamics.
+    "policy_cem"
+    "random_cem"
+    "policy_shooting"
+    "random_shooting"
+# Oracle value / Latent dynamics.
+    # "policy_cem_oracle_value"
+    # "random_cem_oracle_value"
+    # "policy_shooting_oracle_value"
+    # "random_shooting_oracle_value"
+# Q-value / Oracle dynamics.
+    "policy_cem_oracle_dynamics"
+    "random_cem_oracle_dynamics"
+    "policy_shooting_oracle_dynamics"
+    "random_shooting_oracle_dynamics"
+# Oracle value / Oracle dynamics.
+    "policy_cem_oracle_value_dynamics"
+    "random_cem_oracle_value_dynamics"
+    "policy_shooting_oracle_value_dynamics"
+    "random_shooting_oracle_value_dynamics"
 # Random.
+    "random"
+)
 
-PLANNER_CONFIG=configs/pybox2d/planners/random.yaml
-ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
-POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
-DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
-eval_planner
+# Evaluate planners.
+
+for planner in "${PLANNERS[@]}"; do
+    ENV_CONFIG=configs/pybox2d/envs/placeright_pushleft.yaml
+    POLICY_CHECKPOINTS="models/placeright/final_model.pt models/pushleft/final_model.pt"
+    if [[ "${planner}" == *_oracle_*dynamics ]]; then
+        DYNAMICS_CHECKPOINT=""
+    else
+        DYNAMICS_CHECKPOINT="models/placeright_pushleft/final_model.pt"
+    fi
+    PLANNER_CONFIG="configs/pybox2d/planners/${planner}.yaml"
+
+    eval_planner
+done
+
+if [[ `hostname` == "sc.stanford.edu" ]]; then
+    exit
+fi
+
+# Visualize results.
 
 visualize_results
