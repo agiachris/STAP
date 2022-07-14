56b262557602cc7eed58e86138da060e5700a3ec
agiachris <christopheragia@gmail.com>
2022-07-14 08:08:08 -0700

    add decoupled_state exp

--- scripts/train/train_policies.sh
+++ scripts/train/train_policies.sh
@@ -17,8 +17,8 @@ function train_policy {
     args="${args} --trainer-config ${TRAINER_CONFIG}"
     args="${args} --agent-config ${AGENT_CONFIG}"
     args="${args} --env-config ${ENV_CONFIG}"
-    args="${args} --encoder-checkpoint ${ENCODER_CHECKPOINT}"
-    args="${args} --path models/${EXP_NAME}"
+    # args="${args} --encoder-checkpoint ${ENCODER_CHECKPOINT}"
+    args="${args} --path ${OUTPUTS}/models/${EXP_NAME}"
     args="${args} --seed 0"
     # args="${args} --overwrite"
 
@@ -27,12 +27,12 @@ function train_policy {
 }
 
 # EXP_NAME="20220428/decoupled_state_debug"
-#
+
 # TRAINER_CONFIG="configs/pybox2d/trainers/agent.yaml"
 # AGENT_CONFIG="configs/pybox2d/agents/sac.yaml"
 # ENV_CONFIG="configs/pybox2d/envs/placeright.yaml"
 # train_policy
-#
+
 # TRAINER_CONFIG="configs/pybox2d/trainers/agent.yaml"
 # AGENT_CONFIG="configs/pybox2d/agents/sac.yaml"
 # ENV_CONFIG="configs/pybox2d/envs/pushleft.yaml"
@@ -50,15 +50,27 @@ function train_policy {
 # ENV_CONFIG="configs/pybox2d/envs/pushleft_img.yaml"
 # train_policy
 
-EXP_NAME="20220510/vae"
+# EXP_NAME="20220510/vae"
 
-TRAINER_CONFIG="configs/pybox2d/trainers/agent.yaml"
-AGENT_CONFIG="configs/pybox2d/agents/sac_vae.yaml"
-ENV_CONFIG="configs/pybox2d/envs/placeright_img.yaml"
-ENCODER_CHECKPOINT="models/20220510/vae/encoder/final_model.pt"
-train_policy
+# TRAINER_CONFIG="configs/pybox2d/trainers/agent.yaml"
+# AGENT_CONFIG="configs/pybox2d/agents/sac_vae.yaml"
+# ENV_CONFIG="configs/pybox2d/envs/placeright_img.yaml"
+# ENCODER_CHECKPOINT="models/20220510/vae/encoder/final_model.pt"
+# train_policy
+
+# TRAINER_CONFIG="configs/pybox2d/trainers/agent.yaml"
+# AGENT_CONFIG="configs/pybox2d/agents/sac_vae.yaml"
+# ENV_CONFIG="configs/pybox2d/envs/pushleft_img.yaml"
+# train_policy
+
+EXP_NAME="20220714/decoupled_state"
 
 TRAINER_CONFIG="configs/pybox2d/trainers/agent.yaml"
 AGENT_CONFIG="configs/pybox2d/agents/sac.yaml"
-ENV_CONFIG="configs/pybox2d/envs/pushleft_img.yaml"
+ENV_CONFIG="configs/pybox2d/envs/placeright.yaml"
 train_policy
+
+# TRAINER_CONFIG="configs/pybox2d/trainers/agent.yaml"
+# AGENT_CONFIG="configs/pybox2d/agents/sac.yaml"
+# ENV_CONFIG="configs/pybox2d/envs/pushleft.yaml"
+# train_policy
