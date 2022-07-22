b44acc6415ec4f8a0accb5ce8cca5416ad4a4e56
agiachris <christopheragia@gmail.com>
2022-07-22 14:37:53 -0400

    add support for scod checkpoint

--- temporal_policies/trainers/utils.py
+++ temporal_policies/trainers/utils.py
@@ -1,7 +1,7 @@
 import pathlib
 from typing import Any, Dict, Optional, Sequence, Union
 
-from temporal_policies import agents, dynamics, encoders, trainers, scod
+from temporal_policies import agents, dynamics, encoders, trainers, scod as scod_regression
 from temporal_policies.dynamics import load as load_dynamics
 from temporal_policies.dynamics import LatentDynamics, load_policy_checkpoints
 from temporal_policies.utils import configs
@@ -17,7 +17,7 @@ class TrainerFactory(configs.Factory):
         agent: Optional[agents.RLAgent] = None,
         dynamics: Optional[dynamics.LatentDynamics] = None,
         encoder: Optional[encoders.Encoder] = None,
-        scod: Optional[scod.SCOD] = None,
+        scod: Optional[scod_regression.SCOD] = None,
         checkpoint: Optional[Union[str, pathlib.Path]] = None,
         policy_checkpoints: Optional[Sequence[Union[str, pathlib.Path]]] = None,
         agent_trainers: Optional[Sequence["trainers.AgentTrainer"]] = None,
@@ -105,6 +105,13 @@ class TrainerFactory(configs.Factory):
 
             self.kwargs["policy_checkpoints"] = policy_checkpoints
         elif issubclass(self.cls, trainers.SCODTrainer):
+            if scod is None:
+                if checkpoint is None:
+                    raise ValueError("EIther scod or checkpoint must be specified")
+                ckpt_scod = scod_regression.load(checkpoint=checkpoint, device=device)
+                if not isinstance(ckpt_scod, scod_regression.SCOD):
+                    raise ValueError("Checkpoint scod must be a SCOD instance")
+                scod = ckpt_scod            
             self.kwargs["scod"] = scod
             if len(policy_checkpoints) != 1:
                 raise ValueError("Must specify exactly one policy checkpoint")
