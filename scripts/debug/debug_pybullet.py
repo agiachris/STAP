74fbf03fabb7003576d9a7d43c8bdac10f748271
agiachris <christopheragia@gmail.com>
2022-09-01 14:51:26 -0700

    extend debugger for sequential tasks
    Signed-off-by: agiachris <christopheragia@gmail.com>

--- scripts/debug/debug_pybullet.py
+++ scripts/debug/debug_pybullet.py
@@ -11,26 +11,28 @@ from temporal_policies.utils import timing
 
 def main(env_config: str) -> None:
     env_factory = envs.EnvFactory(config=env_config)
-    env = env_factory()
+    env: pybullet.table_env.TableEnv  = env_factory()
     timer = timing.Timer()
 
     while True:
-        timer.tic("reset")
         obs = env.reset()
-        print("obs:", obs)
-        input("continue?")
-        dt_reset = timer.toc("reset")
+        action_skeleton = env.task.action_skeleton
+        done = False
+        for step in range(len(action_skeleton)):
+            # Set and get primitive
+            env.set_primitive(primitive=action_skeleton[step])
+            primitive = env.get_primitive()
+            input(f"Execute primitive: {primitive}")
 
-        timer.tic("step")
-        primitive = env.get_primitive()
-        assert isinstance(primitive, pybullet.table.primitives.Primitive)
-        action = primitive.sample_action()
-        obs, success, _, _, _ = env.step(primitive.normalize_action(action.vector))
-        dt_step = timer.toc("step")
+            # Sample action and step environment
+            action = primitive.sample_action()
+            obs, success, _, truncated, _ = env.step(primitive.normalize_action(action.vector))    
+            print(f"Success {primitive}: {success}")
+            if truncated:
+                break
 
-        print(f"SUCCESS {primitive}:", success, ", time:", dt_reset + dt_step)
-        input("continue?")
-
+        input("Done task, continue?\n")
+            
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
