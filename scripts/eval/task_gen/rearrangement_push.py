1a48762b2f78279480d5f952dea68617b4f5cdab
agiachris <christopheragia@gmail.com>
2022-09-08 00:15:56 -0700

    final RearrangementPush tasks and taskgen
    Signed-off-by: agiachris <christopheragia@gmail.com>

--- scripts/eval/task_gen/rearrangement_push.py
+++ scripts/eval/task_gen/rearrangement_push.py
@@ -1,149 +1,89 @@
-from typing import Any, List, Dict, Tuple, Optional
+from typing import Any, List, Dict
 
-import yaml
-import random
 import argparse
 
 from utils import (
     OBJECTS,
-    LOCATIONS,
     LIFTED_OBJECTS,
-    get_random_indices,
-    get_lifted_task,
-    substitute_vars,
-    sort_propositions,
+    generate_ground_tasks,
 )
 
 
-def hook_reach_task_phase(
-    phase: int,
-    objects: List[str],
-    init_location_hook: str,
-    target_location_box: Optional[str],
-    on_rack_table: bool,
-) -> Tuple[List[str], List[str]]:
-    """Generate phase of the hook reach task.
-
-    Args:
-        phase: index of the task phase.
-        objects: List of (optionally grounded) box names for the task.
-        init_location_hook: Initial hook location in ["table", "rack", "bin"].
-        target_location_box: Placement location of task-relevant boxes, e.g., "rack".
-        on_rack_table: Whether or not the rack exists in the environment.
-
-    Returns:
-        phase_skeleton: Plan skeleton of this phase.
-        phase_predicates: Predicates implied by the phase skeleton.
-    """
-    arg_object = objects[phase]
-
-    phase_skeleton = []
-    if phase > 0:
-        init_location_hook = "table"
-        if target_location_box is None:
-            phase_skeleton.append(f"place({arg_object}, table)")
-
-    phase_skeleton.extend(
-        [
-            f"pick(hook, {init_location_hook})",
-            f"pull({arg_object}, hook)",
-            "place(hook, table)",
-            f"pick({arg_object}, table)",
-        ]
-    )
-    if target_location_box is not None:
-        phase_skeleton.append(f"place({arg_object}, {target_location_box})")
-
-    phase_predicates = [
-        f"free({arg_object})",
-        f"beyondworkspace({arg_object})",
-        f"on({arg_object}, table)",
-    ]
-    if on_rack_table:
-        phase_predicates.append(f"nonblocking({arg_object}, rack)")
-
-    return phase_skeleton, phase_predicates
-
-
-def hook_reach_task(
+def rearrangement_push_task(
     num_tasks: int,
     objects: List[str],
-    locations: List[str],
     num_arg_objects: int,
     num_non_arg_objects: int = 0,
-    init_location_hook: str = "table",
-    target_location_box: Optional[str] = None,
-    on_rack_table: bool = False,
+    arg_object_zone: str = "inoperationalzone",
+    non_arg_object_zone: str = "inobstructionzone",
     poslimit_rack: bool = False,
     aligned_rack: bool = False,
 ) -> Dict[str, Any]:
-    """Generate a hook reach lifted task.
+    """Generate a rearrangement push lifted task.
 
     Args:
         num_tasks: Number of ground task instances to sample.
         objects: List of (optionally grounded) box names for the task.
-        locations: Valid placement locations for the objects.
         num_arg_objects: Number of task-relevant boxes to be pulled.
         num_non_arg_objects: Number of task-irrelevant boxes in the environment.
-        init_location_hook: Initial hook location in ["table", "rack", "bin"].
-        target_location_box: Placement location of task-relevant boxes, e.g., "rack".
-        on_rack_table: Whether or not the rack exists in the environment.
+        arg_object_zone: TableBounds predicate arg-object initial zone.
+        non_arg_object_zone: TableBounds predicate for non-arg-object initial zone.
         poslimit_rack: Whether or not the rack is at fixed locations.
+        aligned_rack: Whether or not the rack frame aligns with the world frame.
 
     Returns:
         lifted_task: Plan skeleton and initial state of the task
                      with additional metadata to guide sampling.
     """
-    assert init_location_hook in LOCATIONS and num_arg_objects >= 1
+    assert num_arg_objects >= 1
     assert num_arg_objects + num_non_arg_objects <= len(objects)
 
     # Base task predicates
-    inworkspace_rack = init_location_hook == "rack" or target_location_box == "rack"
-    on_rack_table = on_rack_table or inworkspace_rack
-    if poslimit_rack and not on_rack_table:
-        raise ValueError("Cannot apply poslimit(rack) without rack")
-
-    predicates = ["free(hook)", "inworkspace(hook)", f"on(hook, {init_location_hook})"]
-    if on_rack_table:
-        predicates.append("on(rack, table)")
-    if inworkspace_rack:
-        predicates.append("inworkspace(rack)")
+    predicates = [
+        "free(hook)",
+        "inworkspace(hook)",
+        "beyondworkspace(rack)",
+        "on(rack, table)",
+        "on(hook, table)",
+    ]
     if poslimit_rack:
         predicates.append("poslimit(rack)")
     if aligned_rack:
         predicates.append("aligned(rack)")
 
-    # Construct plan skeleton and arg-object predicates
+    # Construct plan skeleton and non-arg-object predicates
     plan_skeleton = []
-    for arg_idx in range(num_arg_objects):
-        phase_skeleton, phase_predicates = hook_reach_task_phase(
-            phase=arg_idx,
-            objects=objects,
-            init_location_hook=init_location_hook,
-            target_location_box=target_location_box,
-            on_rack_table=on_rack_table,
-        )
-        plan_skeleton.extend(phase_skeleton)
-        predicates.extend(phase_predicates)
-        for _arg_idx in range(num_arg_objects):
-            if _arg_idx != arg_idx:
-                predicates.append(
-                    f"nonblocking({objects[arg_idx]}, {objects[_arg_idx]})"
-                )
-
-    # Assign non-arg-object On and NonBlocking predicates
     for non_arg_idx in range(num_non_arg_objects):
-        non_arg_object = objects[num_arg_objects + non_arg_idx]
-        non_arg_location = (
-            locations[random.randint(0, len(locations) - 1)]
-            if on_rack_table
-            else "table"
+        non_arg_object = objects[non_arg_idx]
+        plan_skeleton.extend(
+            [
+                f"pick({non_arg_object}, table)",
+                f"place({non_arg_object}, table)",
+            ]
         )
-        predicates.append(f"on({non_arg_object}, {non_arg_location})")
-        for arg_idx in range(num_arg_objects):
-            arg_object = objects[arg_idx]
-            if non_arg_location == "table":
-                predicates.append(f"nonblocking({arg_object}, {non_arg_object})")
+        predicates.extend(
+            [
+                f"free({non_arg_object})",
+                f"infront({non_arg_object}, rack)",
+                f"on({non_arg_object}, table)",
+                f"{non_arg_object_zone}({non_arg_object})",
+            ]
+        )
+
+    # Construct plan skeleton and arg-object predicates
+    for arg_idx in range(num_arg_objects):
+        if arg_idx == 0:
+            plan_skeleton.append(f"pick(hook, table)")
+        arg_object = objects[num_non_arg_objects + arg_idx]
+        plan_skeleton.append(f"push({arg_object}, hook)")
+        predicates.extend(
+            [
+                f"free({arg_object})",
+                f"infront({arg_object}, rack)",
+                f"on({arg_object}, table)",
+                f"{arg_object_zone}({arg_object})",
+            ]
+        )
 
     lifted_task = {
         "num_tasks": num_tasks,
@@ -152,40 +92,6 @@ def hook_reach_task(
         "metadata": {"num_lifted_objects": num_arg_objects + num_non_arg_objects},
     }
     return lifted_task
-
-
-def main(
-    objects: Dict[int, str],
-    lifted_tasks: List[Dict[str, Any]],
-) -> None:
-    """Randomly generate an even distribution of lifted evaluation tasks.
-
-    Args:
-        objects: Objects to sample ground tasks with.
-        lifted_tasks: Lifted tasks to ground on provided objects.
-    """
-    num_tasks = sum(task["num_tasks"] for task in lifted_tasks)
-    for task_idx in range(num_tasks):
-        print(f"\nSampling lifted task: {task_idx}")
-
-        # Sample ground objects for hook reach problem
-        lifted_task = get_lifted_task(task_idx, lifted_tasks)
-        num_lifted_objects = lifted_task["metadata"]["num_lifted_objects"]
-        object_idxs = get_random_indices(objects)[:num_lifted_objects]
-        vars = [(f"?M{i}", objects[idx]) for i, idx in enumerate(object_idxs)]
-
-        # Construct action skeleton and initial state
-        action_skeleton = substitute_vars(vars, lifted_task["plan_skeleton"])
-        propositions = substitute_vars(vars, lifted_task["predicates"])
-        initial_state = sort_propositions(propositions)
-        task_config = {
-            "action_skeleton": action_skeleton,
-            "initial_state": initial_state,
-        }
-        print(yaml.dump(task_config, default_flow_style=False, sort_keys=False))
-        input("Continue?")
-
-    print("Done.")
 
 
 if __name__ == "__main__":
@@ -197,65 +103,35 @@ if __name__ == "__main__":
         nargs="*",
         help="Names of ground objects for the task suite.",
     )
-    parser.add_argument(
-        "--locations",
-        "-l",
-        type=str,
-        nargs="*",
-        help="Names of ground placement locations for the task suite.",
-    )
     args = parser.parse_args()
-    assert all(x in OBJECTS for x in args.objects) and all(
-        x in LOCATIONS for x in args.locations
-    )
+    assert all(x in OBJECTS for x in args.objects)
 
     lifted_objects = LIFTED_OBJECTS[: len(args.objects)]
     lifted_tasks = [
-        hook_reach_task(
+        rearrangement_push_task(
             num_tasks=1,
             objects=lifted_objects,
-            locations=args.locations,
             num_arg_objects=1,
-        ),
-        hook_reach_task(
-            num_tasks=1,
-            objects=lifted_objects,
-            locations=args.locations,
-            num_arg_objects=1,
-            num_non_arg_objects=2,
-            on_rack_table=True,
+            num_non_arg_objects=1,
             poslimit_rack=True,
             aligned_rack=True,
         ),
-        hook_reach_task(
-            num_tasks=1,
+        rearrangement_push_task(
+            num_tasks=2,
             objects=lifted_objects,
-            locations=args.locations,
             num_arg_objects=1,
-            num_non_arg_objects=3,
-        ),
-        hook_reach_task(
-            num_tasks=1,
-            objects=lifted_objects,
-            locations=args.locations,
-            num_arg_objects=1,
-            num_non_arg_objects=3,
-            target_location_box="rack",
-            on_rack_table=True,
+            num_non_arg_objects=2,
             poslimit_rack=True,
             aligned_rack=True,
         ),
-        hook_reach_task(
-            num_tasks=1,
+        rearrangement_push_task(
+            num_tasks=2,
             objects=lifted_objects,
-            locations=args.locations,
-            num_arg_objects=2,
-            num_non_arg_objects=2,
-            target_location_box="rack",
-            on_rack_table=True,
+            num_arg_objects=1,
+            num_non_arg_objects=3,
             poslimit_rack=True,
             aligned_rack=True,
         ),
     ]
 
-    main(args.objects, lifted_tasks)
+    generate_ground_tasks(args.objects, lifted_tasks)
--- scripts/eval/task_gen/utils.py
+++ scripts/eval/task_gen/utils.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Tuple, Any
 
+import yaml
 import random
 from symbolic import parse_proposition
 
@@ -15,6 +16,40 @@ OBJECTS = ["blue_box", "yellow_box", "red_box", "cyan_box", "purple_box", "hook"
 LOCATIONS = ["table", "rack", "bin"]
 LIFTED_OBJECTS = ["?M0", "?M1", "?M2", "?M3", "?M4", "?M5"]
 LIFTED_LOCATIONS = ["?L0", "?L1", "?L2"]
+
+
+def generate_ground_tasks(
+    objects: Dict[int, str],
+    lifted_tasks: List[Dict[str, Any]],
+) -> None:
+    """Randomly generate an even distribution of lifted evaluation tasks.
+
+    Args:
+        objects: Objects to sample ground tasks with.
+        lifted_tasks: Lifted tasks to ground on provided objects.
+    """
+    num_tasks = sum(task["num_tasks"] for task in lifted_tasks)
+    for task_idx in range(num_tasks):
+        print(f"\nSampling lifted task: {task_idx}")
+
+        # Sample ground objects for hook reach problem
+        lifted_task = get_lifted_task(task_idx, lifted_tasks)
+        num_lifted_objects = lifted_task["metadata"]["num_lifted_objects"]
+        object_idxs = get_random_indices(objects)[:num_lifted_objects]
+        vars = [(f"?M{i}", objects[idx]) for i, idx in enumerate(object_idxs)]
+
+        # Construct action skeleton and initial state
+        action_skeleton = substitute_vars(vars, lifted_task["plan_skeleton"])
+        propositions = substitute_vars(vars, lifted_task["predicates"])
+        initial_state = sort_propositions(propositions)
+        task_config = {
+            "action_skeleton": action_skeleton,
+            "initial_state": initial_state,
+        }
+        print(yaml.dump(task_config, default_flow_style=False, sort_keys=False))
+        input("Continue?")
+
+    print("Done.")
 
 
 def get_random_indices(objects: Dict[int, str]) -> List[int]:
