65925fb6f6bbee4d65faa43bad8a9839b81ca76a
Kevin Lin <kevin.lin20000@gmail.com>
2022-12-31 21:18:14 -0800

    Add tmux parallelizing policy/Q/dynamics data generation script

---
+++ scripts/data/generate_policy_dataset_tmux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set the base command
+base_command="PYTHONPATH=. python scripts/data/generate_policy_dataset.py \
+--config.num-pretrain-steps 100000 --config.num-train-steps 0 --config.num-eval-episodes 0 --config.gui 0 \
+--config.primitive"
+
+echo "Usage of scripts/data/generate_policy_dataset.py"
+eval PYTHONPATH=. python scripts/data/generate_policy_dataset.py -h
+
+# Set the primitives and seeds
+primitives=("pick" "place" "pull" "push")
+start_seeds=("0" "0" "0" "0")
+final_seeds=("8" "12" "10" "10")
+
+for i in "${!primitives[@]}"; do
+    for seed in $(seq ${start_seeds[$i]} $((${final_seeds[$i]} - 1))); do
+        session_name="primitive-${primitives[$i]}-seed-${seed}"
+        command="${base_command} ${primitives[$i]} --config.seed ${seed}"
+        echo $command
+        tmux new-session -s "$session_name" -d
+        tmux send-keys -t "$session_name" "$command" C-m
+    done
+done
+
+# for each primitive in the list of primitives (pick, place, push, pull) 
+# train run the command for the seed 100
+for primitive in "${primitives[@]}"; do
+    session_name="primitive-${primitive}-seed-100"
+    command="${base_command} ${primitive} --config.seed 100"
+    echo $command
+    tmux new-session -s "$session_name" -d
+    tmux send-keys -t "$session_name" "$command" C-m
+done
