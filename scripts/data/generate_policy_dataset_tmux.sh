8e7f6d8727868582454b75ec6599ec48e272fb58
Kevin Lin <kevin.lin20000@gmail.com>
2022-12-31 21:24:23 -0800

    Add ability to specify conda env for tmux sessions

--- scripts/data/generate_policy_dataset_tmux.sh
+++ scripts/data/generate_policy_dataset_tmux.sh
@@ -5,6 +5,7 @@ base_command="PYTHONPATH=. python scripts/data/generate_policy_dataset.py \
 --config.num-pretrain-steps 100000 --config.num-train-steps 0 --config.num-eval-episodes 0 --config.gui 0 \
 --config.primitive"
 
+echo "Please input the name of the conda environment you want to use for each command"
 echo "Usage of scripts/data/generate_policy_dataset.py"
 eval PYTHONPATH=. python scripts/data/generate_policy_dataset.py -h
 
@@ -16,7 +17,7 @@ final_seeds=("8" "12" "10" "10")
 for i in "${!primitives[@]}"; do
     for seed in $(seq ${start_seeds[$i]} $((${final_seeds[$i]} - 1))); do
         session_name="primitive-${primitives[$i]}-seed-${seed}"
-        command="${base_command} ${primitives[$i]} --config.seed ${seed}"
+        command="conda activate $1 && ${base_command} ${primitives[$i]} --config.seed ${seed}"
         echo $command
         tmux new-session -s "$session_name" -d
         tmux send-keys -t "$session_name" "$command" C-m
@@ -27,7 +28,7 @@ done
 # train run the command for the seed 100
 for primitive in "${primitives[@]}"; do
     session_name="primitive-${primitive}-seed-100"
-    command="${base_command} ${primitive} --config.seed 100"
+    command="conda activate $1 && ${base_command} ${primitive} --config.seed 100"
     echo $command
     tmux new-session -s "$session_name" -d
     tmux send-keys -t "$session_name" "$command" C-m
