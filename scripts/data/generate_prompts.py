6c8687d56be5812abd54e707f9c430e0010c72b7
agiachris <christopheragia@gmail.com>
2023-01-10 15:58:39 -0800

    split taps / t2m env configs
    Signed-off-by: agiachris <christopheragia@gmail.com>

--- scripts/data/generate_prompts.py
+++ scripts/data/generate_prompts.py
@@ -7,27 +7,25 @@ from configs.base_config import *
 def main(config: str):
 
     # Structure Language Task 1.
-    inst = "How would you use the hook to pull the red box and pick it up?"
+    inst = "How would you use the hook to pull the red box closer, then pick it up?"
 
     # Structure Language Task 2.
     inst = "How would you pick the hook up from the rack and place it on the table?"
 
-    # Long-horizon Task 1.
-    inst = "How would you move all the boxes to the rack?"
+    # Structure Language Task 3.
+    inst = "How would you pick and place the cyan box, then use the hook to push the yellow box under the rack?"
 
     # Long-horizon Task 2.
     inst = "How would you move the blue box under the rack?"
-
-    # Long-horizon Task 3.
-    inst = ""
+    
+    # Long-horizon Task 1.
+    inst = "How would you move all of the boxes to the rack?"
 
     # Lifted Task 1.
-    inst = "How would you move boxes to create the most room on the table?"
+    inst = "How would you move the boxes to create the most space on the table?"
 
     # Lifted Task 2.
-    inst = "How would you put a prime colored box on the rack?"
-
-    pass
+    inst = "How would you get a prime colored box on the rack?"
 
 
 if __name__ == "__main__":
