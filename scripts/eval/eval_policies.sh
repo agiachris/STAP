f7f6f14c58560af89f89a2458679dbe2de796e9a
Kevin Lin <kevin.lin20000@gmail.com>
2023-01-10 23:43:30 -0800

    Add finegrained Q statistics for eval policies

--- scripts/eval/eval_policies.sh
+++ scripts/eval/eval_policies.sh
@@ -23,7 +23,7 @@ function eval_policies {
     args="${args} ${ENV_KWARGS}"
     if [[ $DEBUG -ne 0 ]]; then
         args="${args} --path plots/${EXP_NAME}_debug"
-        args="${args} --num-episodes 1"
+        args="${args} --num-episodes 100"
         args="${args} --verbose 1"
     else
         args="${args} --path plots/${EXP_NAME}"
@@ -43,34 +43,49 @@ function eval_policies {
 # Setup.
 
 DEBUG=0
-NUM_EPISODES=100
+NUM_EPISODES=10
 
 # Evaluate policies.
 
 policy_envs=(
-    "pick"
-    "place"
-    "pull"
-    "push"
+    # "pick"
+    "pick_0"
+    # "place_0"
+    # "pull_0"
+    # "push_0"
 )
+# experiments=(
+#     "official"
+# )
+# experiments=(
+#     "20230101/complete_q_multistage"
+# )
 experiments=(
-    "20220908/official"
+    "20230106/complete_q_multistage"
 )
 ckpts=(
     # "best_model"
     # "final_model"
-    # "ckpt_model_50000"
+    # "select_model"
+    # "ckpt_model_550000"
     # "ckpt_model_100000"
     # "ckpt_model_150000"
-    # "ckpt_model_200000"
+    # "ckpt_model_300000"
+    "ckpt_model_1000000"
 )
 if [[ `hostname` == "sc.stanford.edu" ]] || [[ `hostname` == "${GCP_LOGIN}" ]]; then
     ENV_KWARGS="${ENV_KWARGS} --gui 0"
 fi
 
+ENV_KWARGS="${ENV_KWARGS} --gui 0"
+
 for exp_name in "${experiments[@]}"; do
     for ckpt in "${ckpts[@]}"; do
         for policy_env in "${policy_envs[@]}"; do
+            # set ENV_CONFIG to configs/pybullet/envs/official/primitives/<policy_env_name_without_0>_eval.yaml
+            # ENV_CONFIG="configs/pybullet/envs/official/primitives/${policy_env:0:-2}_eval.yaml"
+            # ENV_CONFIG="configs/pybullet/envs/official/primitives/${policy_env:0:-2}_viz.yaml"
+            ENV_CONFIG="configs/pybullet/envs/official/domains/hook_reach/task0.yaml"
             EXP_NAME="${exp_name}/${ckpt}"
             POLICY_CHECKPOINT="models/${exp_name}/${policy_env}/${ckpt}.pt"
             eval_policies
