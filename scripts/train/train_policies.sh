427bd9de80d21c092622a1ee311b075f1c5641d5
agiachris <christopheragia@gmail.com>
2023-01-13 15:59:38 -0800

    prep sac training
    Signed-off-by: agiachris <christopheragia@gmail.com>

--- scripts/train/train_policies.sh
+++ scripts/train/train_policies.sh
@@ -3,12 +3,12 @@
 set -e
 
 # GCP_LOGIN="juno-login-lclbjqwy-001"
-GCP_LOGIN="gcp-login-yq0fvtuw-001"
+# GCP_LOGIN="gcp-login-yq0fvtuw-001"
 
 function run_cmd {
     echo ""
     echo "${CMD}"
-    if [[ `hostname` == "sc.stanford.edu" ]]; then
+    if [[ `hostname` == "sc.stanford.edu" ]] || [[ `hostname` == juno* ]]; then
         sbatch scripts/train/train_juno.sh "${CMD}"
     elif [[ `hostname` == "${GCP_LOGIN}" ]]; then
         sbatch scripts/train/train_gcp.sh "${CMD}"
@@ -32,14 +32,12 @@ function train_policy {
     args="${args} ${ENV_KWARGS}"
     if [[ $DEBUG -ne 0 ]]; then
         args="${args} --path ${POLICY_OUTPUT_PATH}_debug"
-        # args="${args} --eval-recording-path ${EVAL_RECORDING_PATH}_debug"
         args="${args} --overwrite"
         args="${args} --num-pretrain-steps 10"
         args="${args} --num-train-steps 10"
         args="${args} --num-eval-episodes 10"
     else
         args="${args} --path ${POLICY_OUTPUT_PATH}"
-        # args="${args} --num-pretrain-steps 5000"
         args="${args} --eval-recording-path ${EVAL_RECORDING_PATH}"
     fi
 
@@ -53,33 +51,33 @@ output_path="models"
 plots_path="plots"
 
 # Experiments.
+exp_name="20230113/sac"
 
 # Pybullet.
-
-exp_name="20221024/decoupled_state"
 AGENT_CONFIG="configs/pybullet/agents/sac.yaml"
+# AGENT_CONFIG="configs/pybullet/agents/ensemble_sac.yaml"
+TRAINER_CONFIG="configs/pybullet/trainers/agent_sac.yaml"
+# TRAINER_CONFIG="configs/pybullet/trainers/agent_awac.yaml"
 
-TRAINER_CONFIG="configs/pybullet/trainers/agent.yaml"
 POLICY_OUTPUT_PATH="${output_path}/${exp_name}"
 EVAL_RECORDING_PATH="${plots_path}/${exp_name}"
 ENV_KWARGS="--num-env-processes 4 --num-eval-env-processes 2"
-# ENV_KWARGS="${ENV_KWARGS} --use-curriculum 1"
-if [[ `hostname` == "sc.stanford.edu" ]] || [[ `hostname` == "${GCP_LOGIN}" ]]; then
+if [[ `hostname` == "sc.stanford.edu" ]] || [[ `hostname` == "${GCP_LOGIN}" ]] || [[ `hostname` == juno* ]]; then
     ENV_KWARGS="${ENV_KWARGS} --gui 0"
 fi
 
-ENV_CONFIG="configs/pybullet/envs/official/primitives/pick.yaml"
-EVAL_ENV_CONFIG="configs/pybullet/envs/official/primitives/pick_eval.yaml"
+ENV_CONFIG="configs/pybullet/envs/t2m/official/primitives/20230113/primitives_rl/pick.yaml"
+EVAL_ENV_CONFIG="configs/pybullet/envs/t2m/official/primitives/20230113/primitives_rl/pick_eval.yaml"
 train_policy
 
-ENV_CONFIG="configs/pybullet/envs/official/primitives/place.yaml"
-EVAL_ENV_CONFIG="configs/pybullet/envs/official/primitives/place_eval.yaml"
+ENV_CONFIG="configs/pybullet/envs/t2m/official/primitives/20230113/primitives_rl/place.yaml"
+EVAL_ENV_CONFIG="configs/pybullet/envs/t2m/official/primitives/20230113/primitives_rl/place_eval.yaml"
 train_policy
 
-ENV_CONFIG="configs/pybullet/envs/official/primitives/pull.yaml"
-EVAL_ENV_CONFIG="configs/pybullet/envs/official/primitives/pull_eval.yaml"
+ENV_CONFIG="configs/pybullet/envs/t2m/official/primitives/20230113/primitives_rl/pull.yaml"
+EVAL_ENV_CONFIG="configs/pybullet/envs/t2m/official/primitives/20230113/primitives_rl/pull_eval.yaml"
 train_policy
 
-ENV_CONFIG="configs/pybullet/envs/official/primitives/push.yaml"
-EVAL_ENV_CONFIG="configs/pybullet/envs/official/primitives/push_eval.yaml"
+ENV_CONFIG="configs/pybullet/envs/t2m/official/primitives/20230113/primitives_rl/push.yaml"
+EVAL_ENV_CONFIG="configs/pybullet/envs/t2m/official/primitives/20230113/primitives_rl/push_eval.yaml"
 train_policy
