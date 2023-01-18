bceb9d1e94c38e67e99ce053a81db9f97d16ea37
agiachris <christopheragia@gmail.com>
2023-01-18 09:57:56 -0800

    Buggy transformer
    Signed-off-by: agiachris <christopheragia@gmail.com>

---
+++ temporal_policies/networks/transformer.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+from temporal_policies.networks.utils import MultiHeadAttention
+
+
+class AttentionEncoderBlock(nn.Module):
+    """Encoder transformer block."""
+
+    def __init__(
+        self, 
+        attention_heads: int, 
+        embedding_size: int,
+        attention_dropout: float,
+        residual_dropout: float,
+        residual_ratio: int
+    ):
+        """Construct attention encoder block.
+        
+        Args:
+            attention
+        """
+        super().__init__()
+        self._layer_norm = nn.LayerNorm(embedding_size)
+        self._attention = MultiHeadAttention(
+            attention_heads, 
+            embedding_size, 
+            attention_dropout, 
+            residual_dropout
+        )
+        self._residual = nn.Sequential(
+            nn.LayerNorm(embedding_size),
+            nn.Linear(embedding_size, residual_ratio * embedding_size),
+            nn.GELU(approximate="tanh"),
+            nn.Linear(residual_ratio * embedding_size, embedding_size),
+            nn.Dropout(residual_dropout)
+        ) 
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        ln_x = self._layer_norm.forward(x)
+        x = x + self._attention.forward(ln_x, ln_x, ln_x)
+        return x + self._residual.forward(x)
--- temporal_policies/networks/utils.py
+++ temporal_policies/networks/utils.py
@@ -1,4 +1,7 @@
+import math
 import torch
+from torch import nn
+from torch.nn import functional as F
 
 
 class SquashedNormal(torch.distributions.TransformedDistribution):
@@ -15,3 +18,59 @@ class SquashedNormal(torch.distributions.TransformedDistribution):
         for transform in self.transforms:
             loc = transform(loc)
         return loc
+
+
+class MultiHeadAttention(nn.Module):
+    """Vanilla multi-head self-attention later with project at the end."""
+
+    def __init__(
+        self, 
+        n_head: int, 
+        n_embd: int, 
+        attn_pdrop: float, 
+        resid_pdrop: float, 
+    ):
+        """Construct MultiHeadAttention.
+        
+        Args:
+            n_head: Number of attention heads.
+            n_embd: Number of embeddings.
+            attn_pdrop: Attention dropout.
+            resid_pdrop: Residual dropout.
+        """
+        super().__init__()
+        assert n_embd % n_head == 0
+        
+        # Key, query, value projections for all heads.
+        self._key = nn.Linear(n_embd, n_embd)
+        self._query = nn.Linear(n_embd, n_embd)
+        self._value = nn.Linear(n_embd, n_embd)
+        
+        # Regularization.
+        self._attn_drop = nn.Dropout(attn_pdrop)
+        self._resid_drop = nn.Dropout(resid_pdrop)
+
+        # Output projection.
+        self._proj = nn.Linear(n_embd, n_embd)
+        self._n_head = n_head
+
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+        B, T, C = q.size()
+        _, S, C = k.size()
+
+        # Calculate query, key, values for all heads in batch and move head forward to be the batch dim.
+        q = self._query.forward(q).view(B, T, self._n_head, C // self._n_head).transpose(1, 2)  # (B, nh, T, hs)
+        k = self._key.forward(k).view(B, S, self._n_head, C // self._n_head).transpose(1, 2)    # (B, nh, S, hs)
+        v = self._value.forward(v).view(B, S, self._n_head, C // self._n_head).transpose(1, 2)  # (B, nh, S, hs)
+
+        # Causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, S) -> (B, nh, T, S).
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        
+        att = F.softmax(att, dim=-1)
+        att = self._attn_drop.forward(att)
+        y = att @ v  # (B, nh, T, S) x (B, nh, S, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)  # Re-assemble all head outputs side by side.
+
+        # Output projection.
+        y = self._resid_drop(self._proj(y))
+        return y
