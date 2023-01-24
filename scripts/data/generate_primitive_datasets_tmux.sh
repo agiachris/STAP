abdb72b95e2962bc4da4f931cbd47a5c7908578e
agiachris <christopheragia@gmail.com>
2023-01-24 12:50:37 -0800

    add tmux primitive launcher
    Signed-off-by: agiachris <christopheragia@gmail.com>

--- scripts/data/generate_primitive_datasets_tmux.sh
+++ scripts/data/generate_primitive_datasets_tmux.sh
@@ -8,9 +8,8 @@ function run_cmd {
     tmux_name="${SPLIT}_${PRIMITIVE}_${SEED}_${CPU}"
 
     tmux new-session -d -s "${tmux_name}"
-    tmux send-keys -t "${tmux_name}" "taskset -c ${CPU}" Enter
     tmux send-keys -t "${tmux_name}" "export PYTHONPATH=${path_mod}:${PYTHONPATH}" Enter
-    tmux send-keys -t "${tmux_name}" "pipenv run ${PYTHON_CMD}" Enter
+    tmux send-keys -t "${tmux_name}" "taskset -c ${CPU} pipenv run ${PYTHON_CMD}" Enter
 }
 
 function generate_data {
@@ -37,9 +36,9 @@ EXP_NAME="20230124/datasets"
 SYMBOLIC_ACTION_TYPE="valid"
 
 # Pybullet.
-SPLIT="validation"
-SEEDS=($(seq 16 19))
-CPUS=($(seq 0 3))
-PRIMITIVE="pull"
+SPLIT="train"
+SEEDS=($(seq 0 7))
+CPUS=($(seq 4 11))
+PRIMITIVE="push"
 
 run_data_generation
